ca616c5f1714b149c7d95a4b93cad9ade8130f60
richarc <richarc@rose-hulman.edu>
2018-01-25 15:02:38 -0500

    gay

--- sandbox/richarc/motors/m1_drive_timed.py
+++ sandbox/richarc/motors/m1_drive_timed.py
@@ -66,40 +66,6 @@ Authors: David Fisher and Clayton Richards.
 #    print("  Timed Driving")
 #    print("--------------------------------------------")
 #    ev3.Sound.speak("Timed Driving").wait()
-
-import ev3dev.ev3 as ev3
-import time
-
-
-def main():
-    print("--------------------------------------------")
-    print("  Timed Driving")
-    print("--------------------------------------------")
-    ev3.Sound.speak("Timed Driving").wait()
-
-    # Connect two large motors on output ports B and C
-    left_motor = ev3.LargeMotor(ev3.OUTPUT_B)
-    right_motor = ev3.LargeMotor(ev3.OUTPUT_C)
-
-    # Check that the motors are actually connected
-    assert left_motor.connected
-    assert right_motor.connected
-
-    time_s = 1  # Any value other than 0.
-    while time_s != 0:
-        left_sp = int(input("Enter a speed for ): "))
-        right_sp = int(
-            input("Enter a speed for the right motor (0 to 900 dps): "))
-        time_s = int(input("Enter a time to drive (seconds): "))
-        left_motor.run_forever(speed_sp=left_sp)
-        right_motor.run_forever(speed_sp=right_sp)
-        time.sleep(time_s)
-        left_motor.stop()
-        right_motor.stop(stop_action="brake")
-
-    print("Goodbye!")
-    ev3.Sound.speak("Goodbye").wait()
-    
 # Done: 4. Change the input questions from:
 #   Enter a speed for the left motor (0 to 900 dps):
 #   Enter a speed for the right motor (0 to 900 dps):
@@ -125,3 +91,36 @@ def main():
 # TODO: 8. Call over a TA or instructor to sign your team's checkoff sheet and do a code review.
 #
 #  Observation you should make, the pattern run_forever-->time.sleep-->stop naturally blocks code execution until done.
+
+
+import ev3dev.ev3 as ev3
+import time
+
+
+def main():
+    print("--------------------------------------------")
+    print("  Timed Driving")
+    print("--------------------------------------------")
+    ev3.Sound.speak("Timed Driving").wait()
+
+    # Connect two large motors on output ports B and C
+    left_motor = ev3.LargeMotor(ev3.OUTPUT_B)
+    right_motor = ev3.LargeMotor(ev3.OUTPUT_C)
+
+    # Check that the motors are actually connected
+    assert left_motor.connected
+    assert right_motor.connected
+
+    time_s = 1  # Any value other than 0.
+    while time_s != 0:
+        speed = int(input("Enter a speed (0 to 900 dps): "))
+        distance = int(input("Distance to travel (inches): "))
+        distance_inches = 
+        left_motor.run_forever(speed_sp=speed)
+        right_motor.run_forever(speed_sp=speed)
+        time.sleep()
+        left_motor.stop()
+        right_motor.stop(stop_action="brake")
+
+    print("Goodbye!")
+    ev3.Sound.speak("Goodbye").wait()
