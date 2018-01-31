8ce8b7af0cf6f6b5f2ab2098d6835d6ea243f235
richarc <richarc@rose-hulman.edu>
2018-01-31 14:19:33 -0500

    gay

--- sandbox/richarc/motors/m1_drive_timed.py
+++ sandbox/richarc/motors/m1_drive_timed.py
@@ -82,7 +82,8 @@ Authors: David Fisher and Clayton Richards.
 #   You may NOT use the advanced motor commands at this time like: run_to_abs_pos, run_to_rel_pos, or run_timed.
 # DONE: 6. Modify the program so that it will exit immediately if the answer to
 #    any   question is 0.
-# TODO: 7. Formally test your work. When you think you have the problem complete run these tests to be sure:
+# DONE: 7. Formally test your work. When you think you have the problem
+# complete run these tests to be sure:
 #   200 dps 24 inches (make sure it drives within 6 inches of the target distance)
 #   400 dps 24 inches (make sure it drives within 6 inches of the target distance)
 #   800 dps 24 inches (make sure it drives within 6 inches of the target distance)
@@ -90,7 +91,8 @@ Authors: David Fisher and Clayton Richards.
 #   400 dps 36 inches (make sure it drives within 9 inches of the target distance)
 # Do more tests if you see fit.  Ideally you should be +/- 25% of the target goal.
 #
-# TODO: 8. Call over a TA or instructor to sign your team's checkoff sheet and do a code review.
+# DONE: 8. Call over a TA or instructor to sign your team's checkoff sheet and
+# do a code review.
 #
 #  Observation you should make, the pattern run_forever-->time.sleep-->stop naturally blocks code execution until done.
 
@@ -106,7 +108,7 @@ def main():
     ev3.Sound.speak("Timed Driving").wait()
 
     # Connect two large motors on output ports B and C
-    left_motor = ev3.LargeMotor(ev3.OUTPUT_B)
+    left_motor = ev3.LargeMotor(pev3.OUTPUT_B)
     right_motor = ev3.LargeMotor(ev3.OUTPUT_C)
 
     # Check that the motors are actually connected
