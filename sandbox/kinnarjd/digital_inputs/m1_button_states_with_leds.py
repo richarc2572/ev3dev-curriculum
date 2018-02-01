cfbb72303eadd7278124ecd1d8bf76fbcbd60ec8
Jonathan <kinnarjd@rose-hulman.edu>
2018-01-31 22:54:08 -0500

    Working on digital_inputs m1

--- sandbox/kinnarjd/digital_inputs/m1_button_states_with_leds.py
+++ sandbox/kinnarjd/digital_inputs/m1_button_states_with_leds.py
@@ -53,7 +53,7 @@ def main():
 
     current_color_index = 0
     while True:
-        # TODO: 3. Implement the left, right, and up buttons as follows:
+        # Done: 3. Implement the left, right, and up buttons as follows:
         #    When the up button is being pressed:
         #      -- print the word "up"
         #      -- turn off all LEDs
@@ -70,6 +70,18 @@ def main():
         #   Optional: You can also comment out the code above that does the 6 second red, green, off pattern.  It was
         #     there just to provide you with code examples for using the LEDs.  It does not need to run anymore.
         #     Just make sure not to comment out too much. ;)
+        if btn.up:
+            print('up')
+            ev3.Leds.all_off()
+        elif btn.left:
+            print("left")
+            ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.GREEN)
+            ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.BLACK)
+        elif btn.right:
+            print("right")
+            ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.RED)
+            ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.BLACK)
+        time.sleep(1.0)
 
         # TODO: 4. Implement the down button to change the color of both LEDs.
         #   The first press to down should make both LEDs GREEN, the next press makes them RED, then AMBER, then off.
