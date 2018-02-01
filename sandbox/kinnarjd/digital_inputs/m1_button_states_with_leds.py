4e530116281ee268f36b54337ef73a6bcacbfeec
Jonathan <kinnarjd@rose-hulman.edu>
2018-01-31 23:20:25 -0500

    Done with m1

--- sandbox/kinnarjd/digital_inputs/m1_button_states_with_leds.py
+++ sandbox/kinnarjd/digital_inputs/m1_button_states_with_leds.py
@@ -83,13 +83,26 @@ def main():
             ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.BLACK)
         time.sleep(1.0)
 
-        # TODO: 4. Implement the down button to change the color of both LEDs.
+        # Done: 4. Implement the down button to change the color of both LEDs.
         #   The first press to down should make both LEDs GREEN, the next press makes them RED, then AMBER, then off.
         #   If the user presses the down button again, wrap around the list to GREEN and continue as before.
         #   If the user holds down the button, figure out how to make the color change still only happen once.
         #   Since you are only allowed to use states, not event callbacks, this last request is a pain, but it's doable
         #     with a while loop that blocks code execution until the down instance variable is False.
         #     Use a time.sleep(0.01) inside the while loop to do nothing but wait for the button to be released.
+        k = 1
+        if btn.down:
+            color = led_colors[k]
+            ev3.Leds.set_color(ev3.Leds.RIGHT, color)
+            ev3.Leds.set_color(ev3.Leds.LEFT, color)
+            if k >= len(led_colors):
+                k = 0
+            else:
+                k = k + 1
+            time.sleep(0.01)
+        while btn.down is False:
+            time.sleep(0.01)
+
 
         # TODO: 5. Formally test your work. When you think you have the problem complete run these tests:
         #   Press Left - Green left LED is on (try holding the button down for a few seconds when you to the press)
