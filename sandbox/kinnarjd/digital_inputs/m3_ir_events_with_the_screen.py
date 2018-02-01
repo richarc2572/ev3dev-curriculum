390627728e5f71974c0f42214e2bc7d24cf99ee0
richarc <richarc@rose-hulman.edu>
2018-02-01 15:16:12 -0500

    DONE

--- sandbox/kinnarjd/digital_inputs/m3_ir_events_with_the_screen.py
+++ sandbox/kinnarjd/digital_inputs/m3_ir_events_with_the_screen.py
@@ -21,15 +21,16 @@ Which will probably not require you to type the password since sudo was just run
 
 BTW chvt means CHange the Virtual Terminal, and 86ing something means to kick it out.
 
-Authors: David Fisher and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+Authors: David Fisher and Clayton Richards.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import ev3dev.ev3 as ev3
 import time
 from PIL import Image
 
 
-# TODO: 2. Have someone on your team run this program as is on the EV3 and make sure everyone understands the code.
+# DONE: 2. Have someone on your team run this program as is on the EV3 and make
+#  sure everyone understands the code.
 # Can you see what the robot does and explain what each line of code is doing? Talk as a group to make sure.
 
 class DataContainer(object):
@@ -43,15 +44,24 @@ class DataContainer(object):
 
         # All of these images are exactly 178 by 128 pixels, the exact screen resolution
         # They are made by Lego and ship with the Lego Mindstorm EV3 Home Edition software
-        self.eyes = Image.open("/home/robot/csse120/assets/images/ev3_lego/eyes_neutral.bmp")
-        self.angry_eyes = Image.open("/home/robot/csse120/assets/images/ev3_lego/eyes_angry.bmp")
-        self.puppy_dog_eyes = Image.open("/home/robot/csse120/assets/images/ev3_lego/eyes_disappointed.bmp")
-        self.sad_eyes = Image.open("/home/robot/csse120/assets/images/ev3_lego/eyes_hurt.bmp")
-        self.shifty_eyes = Image.open("/home/robot/csse120/assets/images/ev3_lego/eyes_pinch_left.bmp")
-        self.progress_0 = Image.open("/home/robot/csse120/assets/images/ev3_lego/progress_bar_0.bmp")
-        self.progress_50 = Image.open("/home/robot/csse120/assets/images/ev3_lego/progress_bar_50.bmp")
-        self.progress_100 = Image.open("/home/robot/csse120/assets/images/ev3_lego/progress_bar_100.bmp")
-        self.teary_eyes = Image.open("/home/robot/csse120/assets/images/ev3_lego/eyes_tear.bmp")
+        self.eyes = Image.open(
+            "/home/robot/csse120/assets/images/ev3_lego/eyes_neutral.bmp")
+        self.angry_eyes = Image.open(
+            "/home/robot/csse120/assets/images/ev3_lego/eyes_angry.bmp")
+        self.puppy_dog_eyes = Image.open(
+            "/home/robot/csse120/assets/images/ev3_lego/eyes_disappointed.bmp")
+        self.sad_eyes = Image.open(
+            "/home/robot/csse120/assets/images/ev3_lego/eyes_hurt.bmp")
+        self.shifty_eyes = Image.open(
+            "/home/robot/csse120/assets/images/ev3_lego/eyes_pinch_left.bmp")
+        self.progress_0 = Image.open(
+            "/home/robot/csse120/assets/images/ev3_lego/progress_bar_0.bmp")
+        self.progress_50 = Image.open(
+            "/home/robot/csse120/assets/images/ev3_lego/progress_bar_50.bmp")
+        self.progress_100 = Image.open(
+            "/home/robot/csse120/assets/images/ev3_lego/progress_bar_100.bmp")
+        self.teary_eyes = Image.open(
+            "/home/robot/csse120/assets/images/ev3_lego/eyes_tear.bmp")
 
 
 def main():
@@ -69,27 +79,46 @@ def main():
     display_image(dc.lcd_screen, dc.eyes)  # Display an image on the EV3 screen
     ev3.Sound.speak("I R events with the Screen").wait()
 
-    # TODO: 3. Create a remote control object for channel 1. Add lambda callbacks for:
+    # DONE: 3. Create a remote control object for channel 1. Add lambda
+    # callbacks for:
     #   .on_red_up    to call handle_red_up_1    (that exist already) with state and dc as parameters
     #   .on_red_down  to call handle_red_down_1  (that exist already) with state and dc as parameters
     #   .on_blue_up   to call handle_blue_up_1   (that exist already) with state and dc as parameters
     #   .on_blue_down to call handle_blue_down_1 (that exist already) with state and dc as parameters
+    rc1 = ev3.RemoteControl(channel=1)
+    rc1.on_red_up = lambda state: handle_red_up_1(state, dc)
+    rc1.on_red_down = lambda state: handle_red_down_1(state, dc)
+    rc1.on_blue_up = lambda state: handle_blue_up_1(state, dc)
+    rc1.on_blue_down = lambda state: handle_blue_down_1(state, dc)
 
-    # TODO: 5. Create remote control objects for channels 2, 3, and 4. Add lambda callbacks for on_red_up to each one:
+    # DONE: 5. Create remote control objects for channels 2, 3, and 4. Add
+    # lambda callbacks for on_red_up to each one:
     #   Channel 2's .on_red_up should call handle_red_up_2 (that exist already) with state and dc as parameters
     #   Channel 3's .on_red_up should call handle_red_up_3 (that exist already) with state and dc as parameters
     #   Channel 4's .on_red_up should call handle_red_up_4 (that exist already) with state and dc as parameters
+    rc2 = ev3.RemoteControl(channel=2)
+    rc3 = ev3.RemoteControl(channel=3)
+    rc4 = ev3.RemoteControl(channel=4)
+    rc2.on_red_up = lambda state: handle_red_up_2(state, dc)
+    rc3.on_red_up = lambda state: handle_red_up_3(state, dc)
+    rc4.on_red_up = lambda state: handle_red_up_4(state, dc)
 
     # Buttons on EV3
     btn = ev3.Button()
     btn.on_backspace = lambda state: handle_shutdown(state, dc)
 
     while dc.running:
-        # TODO: 4. Call the .process() method on your channel 1 RemoteControl object, then review and run your code.
+        # DONE: 4. Call the .process() method on your channel 1 RemoteControl
+        # object, then review and run your code.
         #   Review the handle functions below to see how they draw to the screen.  They are already finished.
+        rc1.process()
 
-        # TODO: 6. Call the .process() method on your channel 2 - 4 RemoteControl objects and demo your code.
+        # DONE: 6. Call the .process() method on your channel 2 - 4
+        # RemoteControl objects and demo your code.
         #   Review the handle functions below to see how they draw to the screen.  They are already finished.
+        rc2.process()
+        rc3.process()
+        rc4.process()
 
         # TODO: 7. Call over a TA or instructor to sign your team's checkoff sheet and do a code review.
         #
@@ -103,7 +132,8 @@ def main():
     # When the program completes (the user hit the Back button), display a crying image and say goodbye.
     display_image(dc.lcd_screen, dc.teary_eyes)
     ev3.Sound.speak("Goodbye").wait()
-    print("If you ran via SSH and typed 'sudo chvt 6' earlier, don't forget to type")
+    print(
+        "If you ran via SSH and typed 'sudo chvt 6' earlier, don't forget to type")
     print("'sudo chvt 1' to get Brickman back after you finish this program.")
 
 
--- sandbox/kinnarjd/digital_inputs/m4_touch_sensor_arm_movement.py
+++ sandbox/kinnarjd/digital_inputs/m4_touch_sensor_arm_movement.py
@@ -3,15 +3,17 @@
 In this module you will use the touch sensor to make arm movements.  Instead of writing code from scratch you will
 fix the existing code, which is FULL of bugs.
 
-Authors: David Fisher and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+Authors: David Fisher and Clayton Richards.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import ev3dev.ev3 as ev3
 import time
 
 MAX_SPEED = 900
 
-# TODO: 2. Have someone on your team run this program as is on the EV3 and make sure everyone understands the code.
+
+# DONE: 2. Have someone on your team run this program as is on the EV3 and make
+#  sure everyone understands the code.
 # Can you see what the robot does and explain what each line of code is doing? Talk as a group to make sure.
 
 
@@ -28,23 +30,28 @@ def main():
     assert touch_sensor
 
     while True:
-        command_to_run = input("Enter c (for calibrate), u (for up), d (for down), or q (for quit): ")
+        command_to_run = input(
+            "Enter c (for calibrate), u (for up), d (for down), or q (for quit): ")
         if command_to_run == 'c':
             print("Calibrate the arm")
-            print("TODO: 3 is to delete this print statement, uncomment the line below, and implement that function.")
+            print(
+                "TODO: 3 is to delete this print statement, uncomment the line below, and implement that function.")
             # arm_calibration(arm_motor, touch_sensor)
         elif command_to_run == 'u':
             print("Move the arm to the up position")
-            print("TODO: 4 is to delete this print statement, uncomment the line below, and implement that function.")
+            print(
+                "TODO: 4 is to delete this print statement, uncomment the line below, and implement that function.")
             # arm_up(arm_motor, touch_sensor)
         elif command_to_run == 'd':
             print("Move the arm to the down position")
-            print("TODO: 5 is to delete this print statement, uncomment the line below, and implement that function.")
+            print(
+                "TODO: 5 is to delete this print statement, uncomment the line below, and implement that function.")
             # arm_down(arm_motor)
         elif command_to_run == 'q':
             break
         else:
-            print(command_to_run, "is not a known command. Please enter a valid choice.")
+            print(command_to_run,
+                  "is not a known command. Please enter a valid choice.")
 
     print("Goodbye!")
     ev3.Sound.speak("Goodbye").wait()
@@ -73,11 +80,12 @@ def arm_calibration(arm_motor, touch_sensor):
     #   Set the arm encoder position to 0 (the last line below is correct to do that, it's new so no bug there)
 
     # Code that attempts to do this task but has MANY bugs (nearly 1 on every line).  Fix them!
-    arm_motor.run_forever(speed_sp=100)
+    arm_motor.run_forever(speed_sp=MAX_SPEED)
     while not touch_sensor:
         time.sleep(0.01)
-    arm_motor.stop(stop_action="coast")
-
+    arm_motor.stop_action = ev3.Motor.STOP_ACTION_BRAKE
+    arm_motor.stop()
+    ev3.Sound.beep().wait()
     arm_revolutions_for_full_range = 14.2
     arm_motor.run_to_rel_pos(position_sp=-arm_revolutions_for_full_range)
     arm_motor.wait_while(ev3.Motor.STATE_STALLED)
@@ -121,7 +129,8 @@ def arm_down(arm_motor):
 
     # Code that attempts to do this task but has bugs.  Fix them.
     arm_motor.run_to_abs_pos()
-    arm_motor.wait_while(ev3.Motor.STATE_HOLDING)  # Blocks until the motor finishes running
+    arm_motor.wait_while(
+        ev3.Motor.STATE_HOLDING)  # Blocks until the motor finishes running
 
     # TODO: 6. After you fix the bugs in the three arm movement commands demo your code to a TA or instructor.
     #
