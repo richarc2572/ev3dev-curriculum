20be8fdfeb21e4c95649e249149cc0e5abdfd468
richarc <richarc@rose-hulman.edu>
2018-02-05 14:51:25 -0500

    DONE

--- sandbox/richarc/mqtt/m1_pc_shared_circles.py
+++ sandbox/richarc/mqtt/m1_pc_shared_circles.py
@@ -32,7 +32,6 @@ Authors: David Fisher and Clayton Richards.
 import tkinter
 from tkinter import ttk
 
-
 # DONE: 4. Uncomment the code below.  It imports a library and creates a
 # relatively simple class.
 # The constructor receives a Tkinter Canvas and the one and only method draws a circle on that canvas at a given XY.
@@ -40,12 +39,13 @@ from tkinter import ttk
 import mqtt_remote_method_calls as com
 
 
-class mydelegate(object):
+class MyDelegate(object):
     def __init__(self, canvas):
         self.canvas = canvas
 
     def on_circle_draw(self, color, x, y):
-        self.canvas.create_oval(x - 10, y - 10, x + 10, y + 10, fill=color, width=3)
+        self.canvas.create_oval(x - 10, y - 10, x + 10, y + 10, fill=color,
+                                width=3)
 
 
 def main():
@@ -108,13 +108,14 @@ def left_mouse_click(event, mqtt_client):
     # Repeated: If you uncommented the code above to test it, make sure to comment it back out before todo7 below.
 
     # MQTT draw
-    # TODO: 7. Send a message using MQTT that will:
+    # DONE: 7. Send a message using MQTT that will:
     #   - Call the method called "on_circle_draw" on the delegate at the other end of the pipe.
     #   - Pass the parameters [my_color, event.x, event.y] as a list.
     # This is the only TO DO you have to think about.  It is meant to help you learn the mqtt_client.send_message syntax
     # Review the lecture notes about the two parameters passed into the mqtt_client.send_message method if needed
     # All of your teammates should receive the message and create a circle of your color at your click location.
     # Additionally you will receive your own message and draw a circle in your color too.
+    mqtt_client.send_message("on_circle_draw", [my_color, event.x, event.y])
 
     # TODO: 8. Help get everyone on your team running this program at the same time.
     # You should be able to see circles on your computer from everyone else on your team.
