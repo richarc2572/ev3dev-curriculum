6ba9b0dc9384783f6c558f7aaa78488c42d72575
richarc <richarc@rose-hulman.edu>
2018-02-08 14:11:45 -0500

    DONE

--- sandbox/richarc/analog_sensors/color_sensor/m1_drive_to_the_color.py
+++ sandbox/richarc/analog_sensors/color_sensor/m1_drive_to_the_color.py
@@ -21,7 +21,6 @@ import time
 
 import robot_controller as robo
 
-
 # Potential values of the color_sensor.color property
 #   ev3.ColorSensor.COLOR_NOCOLOR is the value 0
 #   ev3.ColorSensor.COLOR_BLACK   is the value 1
@@ -31,7 +30,10 @@ import robot_controller as robo
 #   ev3.ColorSensor.COLOR_RED     is the value 5
 #   ev3.ColorSensor.COLOR_WHITE   is the value 6
 #   ev3.ColorSensor.COLOR_BROWN   is the value 7
-COLOR_NAMES = ["None", "Black", "Blue", "Green", "Yellow", "Red", "White", "Brown"]
+COLOR_NAMES = ["None", "Black", "Blue", "Green", "Yellow", "Red", "White",
+               "Brown"]
+
+
 # This list is just a helper list if you ever want the string (for printing or speaking) from a color value.
 
 
@@ -58,11 +60,16 @@ def main():
 
     # For our standard shutdown button.
     btn = ev3.Button()
-    # TODO: 2. Uncomment the lines below to setup event handlers for these buttons.
-    # btn.on_up = lambda state: drive_to_color(state, robot, ev3.ColorSensor.COLOR_RED)
-    # btn.on_down = lambda state: drive_to_color(state, robot, ev3.ColorSensor.COLOR_BLUE)
-    # btn.on_left = lambda state: drive_to_color(state, robot, ev3.ColorSensor.COLOR_BLACK)
-    # btn.on_right = lambda state: drive_to_color(state, robot, ev3.ColorSensor.COLOR_WHITE)
+    # DONE: 2. Uncomment the lines below to setup event handlers for these
+    # buttons.
+    btn.on_up = lambda state: drive_to_color(state, robot,
+                                             ev3.ColorSensor.COLOR_RED)
+    btn.on_down = lambda state: drive_to_color(state, robot,
+                                               ev3.ColorSensor.COLOR_BLUE)
+    btn.on_left = lambda state: drive_to_color(state, robot,
+                                               ev3.ColorSensor.COLOR_BLACK)
+    btn.on_right = lambda state: drive_to_color(state, robot,
+                                                ev3.ColorSensor.COLOR_WHITE)
     btn.on_backspace = lambda state: handle_shutdown(state, dc)
 
     while dc.running:
@@ -94,8 +101,6 @@ def drive_to_color(button_state, robot, color_to_seek):
         #   self.color_sensor = ev3.ColorSensor()
         #   assert self.color_sensor
         # Then here you can use a command like robot.color_sensor.color to check the value
-
-
 
         # TODO: 4. Call over a TA or instructor to sign your team's checkoff sheet.
         #
