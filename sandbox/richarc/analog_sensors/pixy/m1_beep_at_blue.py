1ad0b7dabbd013f163c18a5304084cbd24d2a155
Jonathan <kinnarjd@rose-hulman.edu>
2018-02-12 15:08:54 -0500

    .

--- sandbox/richarc/analog_sensors/pixy/m1_beep_at_blue.py
+++ sandbox/richarc/analog_sensors/pixy/m1_beep_at_blue.py
@@ -42,7 +42,7 @@ def main():
     print("Goodbye!")
     ev3.Sound.speak("Goodbye").wait()
 
-# TODO: 3. Call over a TA or instructor to sign your team's checkoff sheet.
+# Done: 3. Call over a TA or instructor to sign your team's checkoff sheet.
 #
 # Observations you should make, the Pixy cam can detect colors.  That's just neat. ;)
 
