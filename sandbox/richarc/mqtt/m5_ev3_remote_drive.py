ab3f32c80e6aedcb10d30c945d28080fc575a398
Jonathan <kinnarjd@rose-hulman.edu>
2018-02-14 14:03:35 -0500

    Individual Project Work

--- sandbox/richarc/mqtt/m5_ev3_remote_drive.py
+++ sandbox/richarc/mqtt/m5_ev3_remote_drive.py
@@ -41,8 +41,4 @@ def main():
     robot.loop_forever()  # Calls a function that has a while True: loop
     # within it to avoid letting the program end.
 
-
-# ----------------------------------------------------------------------
-# Calls  main  to start the ball rolling.
-# ----------------------------------------------------------------------
 main()
