a13c5dae5631256065ccab448292882934223e96
tomlingb <tomlingb@rose-hulman.edu>
2018-02-14 13:56:48 -0500

    ldiuwbej

--- sandbox/kinnarjd/analog_sensors/color_sensor/m1_drive_to_the_color.py
+++ sandbox/kinnarjd/analog_sensors/color_sensor/m1_drive_to_the_color.py
@@ -13,7 +13,7 @@ If the user presses the Down  button, the robot drives until the robot gets to B
 If the user presses the Left  button, the robot drives until the robot gets to Black.
 If the user presses the Right button, the robot drives until the robot gets to White.
 
-Authors: David Fisher and Jonathan Kinnard.
+Authors: David Fisher and Geoffrey T.
 """  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import ev3dev.ev3 as ev3
