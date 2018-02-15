e705c2eaf78383a792f719b405f99e7ce936648b
Jonathan <kinnarjd@rose-hulman.edu>
2018-02-14 19:58:35 -0500

    Individual Project Work

--- sandbox/richarc/mqtt/m5_ev3_remote_drive.py
+++ sandbox/richarc/mqtt/m5_ev3_remote_drive.py
@@ -31,14 +31,17 @@ Author: David Fisher.
 """
 import mqtt_remote_method_calls as com
 import robot_controller as robo
+import ev3dev.ev3 as ev3
 
 
 def main():
+    ev3.Sound.speak("Remote Drive").wait()
     robot = robo.Snatch3r()
     mqtt_client = com.MqttClient(robot)
     mqtt_client.connect_to_pc()
     # mqtt_client.connect_to_pc("35.194.247.175")  # Off campus IP address of a GCP broker
     robot.loop_forever()  # Calls a function that has a while True: loop
     # within it to avoid letting the program end.
+
 
 main()
