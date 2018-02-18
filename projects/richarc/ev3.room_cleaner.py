06513c1275c10159a1019d950a5fcef8e3f354a9
richarc <richarc@rose-hulman.edu>
2018-02-18 13:13:56 -0500

    DONE

---
+++ projects/richarc/ev3.room_cleaner.py
@@ -0,0 +1,7 @@
+import mqtt_remote_method_calls as com
+
+import ev3dev.ev3 as ev3
+import time
+
+class MyDelegate(object):
+    
--- projects/richarc/pc.room_cleaner.py
+++ projects/richarc/pc.room_cleaner.py
@@ -14,5 +14,19 @@ def main():
     main_frame = ttk.Frame(root, padding=20, relief='raised')
     main_frame.grid()
 
+    message_label = ttk.Label(main_frame, text="--")
+    message_label.grid(row=0, column=0)
+
+    clean_button = ttk.Button(main_frame, text="Clean Room")
+    clean_button.grid(row=1, column=0)
+    clean_button['command'] = lambda: send_clean_room(mqtt_client)
+
+    root.mainloop()
+
+
+def send_clean_room(mqtt_client):
+    print("Telling ev3 to clean room")
+    mqtt_client.send_message("clean_room")
+
 
 main()
