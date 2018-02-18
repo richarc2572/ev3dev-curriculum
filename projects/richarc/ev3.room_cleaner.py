592e71502d5b7010500fc0a40eef371214a44305
richarc <richarc@rose-hulman.edu>
2018-02-18 13:57:20 -0500

    DONE

--- projects/richarc/ev3.room_cleaner.py
+++ projects/richarc/ev3.room_cleaner.py
@@ -1,5 +1,5 @@
 import mqtt_remote_method_calls as com
-
+import robot_controller as robo
 import ev3dev.ev3 as ev3
 import time
 
@@ -8,14 +8,31 @@ class MyDelegate(object):
 
     def __init__(self):
         self.running = False
+        self.room_is_clean = True
+        self.robot = robo.Snatch3r()
+        self.mqtt_client = None
 
     def clean_room(self):
-        print("Cleaning room")
+        if self.room_is_clean:
+            print("Room is already clean")
+            ev3.Sound.speak("Room is already clean")
+        else:
+            print("Cleaning room")
+            ev3.Sound.speak("Cleaning room")
+            self.room_is_clean = True
+
+    def is_room_clean(self):
+        self.mqtt_client.send_message("is_room_clean", [self.room_is_clean])
 
     def loop_forever(self):
         self.running = True
         btn = ev3.Button()
+        self.robot.pixy.mode = "SIG1"
         while not btn.backspace and self.running:
+            if self.robot.pixy.value(1) == 0:
+                self.room_is_clean = True
+            else:
+                self.room_is_clean = False
             time.sleep(0.01)
         print("Goodbye")
         ev3.Sound.speak("Goodbye").wait()
@@ -27,6 +44,7 @@ def main():
     my_delegate = MyDelegate()
     mqtt_client = com.MqttClient(my_delegate)
     mqtt_client.connect_to_pc()
+    my_delegate.mqtt_client = mqtt_client
     my_delegate.loop_forever()
 
 
