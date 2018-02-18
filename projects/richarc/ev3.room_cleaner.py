d8d28ec20f774d91fc9e5e5b00060dc0ded6687e
richarc <richarc@rose-hulman.edu>
2018-02-18 13:31:00 -0500

    DONE

--- projects/richarc/ev3.room_cleaner.py
+++ projects/richarc/ev3.room_cleaner.py
@@ -3,5 +3,27 @@ import mqtt_remote_method_calls as com
 import ev3dev.ev3 as ev3
 import time
 
+
 class MyDelegate(object):
-    
+
+    def clean_room(self):
+        print("Cleaning room")
+
+    def loop_forever(self):
+        btn = ev3.Button()
+        while not btn.backspace:
+            time.sleep(0.01)
+        
+
+
+
+def main():
+    ev3.Sound.speak("Room Cleaner").wait()
+
+    my_delegate = MyDelegate()
+    mqtt_client = com.MqttClient(my_delegate)
+    mqtt_client.connect_to_pc()
+    my_delegate.loop_forever()
+
+
+main()
