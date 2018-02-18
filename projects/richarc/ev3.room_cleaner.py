df6c6b605cbc988d7a88365659faf45cbc68012e
richarc <richarc@rose-hulman.edu>
2018-02-18 13:41:49 -0500

    DONE

--- projects/richarc/ev3.room_cleaner.py
+++ projects/richarc/ev3.room_cleaner.py
@@ -6,15 +6,19 @@ import time
 
 class MyDelegate(object):
 
+    def __init__(self):
+        self.running = False
+
     def clean_room(self):
         print("Cleaning room")
 
     def loop_forever(self):
+        self.running = True
         btn = ev3.Button()
-        while not btn.backspace:
+        while not btn.backspace and self.running:
             time.sleep(0.01)
-        
-
+        print("Goodbye")
+        ev3.Sound.speak("Goodbye").wait()
 
 
 def main():
